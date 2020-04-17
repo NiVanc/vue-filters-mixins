0d82509017152da1e9d5d720f0f098a9ddddeb4d
NiVanc <nico@izebra.be>
2020-04-17 15:54:40 +0200

    Create a list component

--- src/List.vue
+++ src/List.vue
@@ -1,22 +1,15 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Filters & Mixins</h1>
-        <p>{{ text | toUppercase | toLowercase }}</p>
-        <input v-model="filterText" />
-        <ul>
-          <li v-for="fruit in filteredFruits">{{ fruit }}</li>
-        </ul>
-      </div>
+  <div class="row">
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+      <input v-model="filterText" />
+      <ul>
+        <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+      </ul>
     </div>
-    <app-list></app-list>
   </div>
 </template>
 
 <script>
-import List from "./List.vue";
-
 export default {
   data() {
     return {
@@ -36,9 +29,6 @@ export default {
         return element.match(this.filterText);
       });
     }
-  },
-  components: {
-    "app-list": List
   }
 };
 </script>
