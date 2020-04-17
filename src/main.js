a2c29f96c0638c8baee8a7d2fba2e2a089c82466
NiVanc <nico@izebra.be>
2020-04-17 16:19:32 +0200

    Create a global mixin

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ Vue.filter("toLowercase", (value) => {
   return value.toLowerCase();
 });
 
+Vue.mixin({
+  created() {
+    console.log("Global mixin - Created hook");
+  },
+});
+
 new Vue({
   el: "#app",
   render: (h) => h(App),
